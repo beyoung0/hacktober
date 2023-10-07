4cc22b7387a7b00f21999d6ddab8220222473697
Tanmay Gupta <71523770+tanmaygupta01@users.noreply.github.com>
2023-10-07 10:21:21 +0530

    Update hi.cpp
    Added sum function

--- hi.cpp
+++ hi.cpp
@@ -1,8 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int sum(int a,int b){
+  return a+b;
+}
+
 int main(){
   int a = 8;
   int b = 9;
   cout<<"Sum is: "<<a+b<<endl;
+  cout<<"Sum is: "<<sum(a,b)<<endl;
 }
